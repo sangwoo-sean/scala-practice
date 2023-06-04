7d14c3a6f0604210f664b353ac9bc530277c42b6
sangwoo <tkddn3367@gmail.com>
2023-06-04 22:27:51 +0900

    status app val to function

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,7 +2,9 @@ import zhttp.service._
 import zio._
 
 object MainApp extends ZIOAppDefault {
-  def run =
-//    Server.start(port = 8080, http = GreetingApp() ++ StatusApp.app)
-    Server.start(port = 8080, http = GreetingApp() ++ RequestApp() ++ StatusApp.app)
+  def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] =
+    Server.start(
+      port = 8080,
+      http = GreetingApp() ++ RequestApp() ++ StatusApp()
+    )
 }
--- src/main/scala/Status.scala
+++ src/main/scala/Status.scala
@@ -1,8 +1,9 @@
 import zhttp.http._
 
 object StatusApp {
-  val app: Http[Any, Nothing, Request, Response] = Http.collect[Request] {
-    case Method.GET -> _ / "ok" => Response.ok
-    case Method.GET -> _ / "redirect" => Response.redirect("ok")
-  }
+  def apply(): Http[Any, Nothing, Request, Response] =
+    Http.collect[Request] {
+      case Method.GET -> _ / "ok" => Response.ok
+      case Method.GET -> _ / "redirect" => Response.redirect("ok")
+    }
 }
