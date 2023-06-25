f520a9c0fe144aea9012be9ce9aac6f5adaff450
sangwoo <tkddn3367@gmail.com>
2023-06-25 22:47:50 +0900

    change it into ZIO

--- src/main/scala/Greet.scala
+++ src/main/scala/Greet.scala
@@ -1,33 +1,54 @@
+import com.sun.net.httpserver.HttpServer
 import zhttp.http._
+import zhttp.service.Server
+import zio.{ZIO, ZIOAppDefault, ZLayer}
 
 /** An http app that:
  *   - Accepts a `Request` and returns a `Response`
  *   - Does not fail
  *   - Does not use the environment
  */
-object GreetingApp {
-  def apply(): Http[Any, Nothing, Request, Response] =
-    Http.collect[Request] {
-      // GET /greet?name=:name
-      case req@(Method.GET -> _ / "greet")
-        if (req.url.queryParams.nonEmpty) =>
-        Response.text(s"Hello ${req.url.queryParams("name").mkString(" and ")}!")
+object Main extends ZIOAppDefault {
 
-      // GET /greet
-      case Method.GET -> _ / "greet" =>
-        Response.text(s"Hello World!")
+  val prog = for {
+    server <- ZIO.service[HttpServer]
+    _ <- Server.start(8080, server.httpApp)
+  } yield ()
 
-      // GET /greet/:name
-      case Method.GET -> _ / "greet" / name =>
-        Response.text(s"Hello $name!")
 
-      case Method.GET -> _ / "fruits" / "a" =>
-        Response.text("Apple")
+  override def run = {
+    prog.provide(HttpServer.layer)
+  }
 
-      case Method.GET -> _ / "fruits" / "b" =>
-        Response.text("banana")
+}
 
-      case Method.GET -> _ / "Apple" / int(count) =>
-        Response.text(s"Apple: $count")
-    }
+class HttpServer() {
+  val httpApp = Http.collect[Request] {
+    // GET /greet?name=:name
+    case req@(Method.GET -> _ / "greet")
+      if (req.url.queryParams.nonEmpty) =>
+      Response.text(s"Hello ${req.url.queryParams("name").mkString(" and ")}!")
+
+    // GET /greet
+    case Method.GET -> _ / "greet" =>
+      Response.text(s"Hello World!")
+
+    // GET /greet/:name
+    case Method.GET -> _ / "greet" / name =>
+      Response.text(s"Hello $name!")
+
+    case Method.GET -> _ / "fruits" / "a" =>
+      Response.text("Apple")
+
+    case Method.GET -> _ / "fruits" / "b" =>
+      Response.text("banana")
+
+    case Method.GET -> _ / "Apple" / int(count) =>
+      Response.text(s"Apple: $count")
+  }
+}
+
+
+object HttpServer {
+  val layer = ZLayer.succeed(new HttpServer())
 }
