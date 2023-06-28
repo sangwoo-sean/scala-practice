12908a4aa9e774273305406cbf695f4cca956cf9
sangwoo <tkddn3367@gmail.com>
2023-06-28 23:10:44 +0900

    scalatag inline styles

--- src/main/scala/Home.scala
+++ src/main/scala/Home.scala
@@ -6,11 +6,17 @@ object Home {
       head(
         script("some script")
       ),
-      body(
+      body(backgroundColor:="#404040", color:="#dddddd")(
         h1("This is my title"),
         div(
           p("This is my first paragraph"),
           p("This is my second paragraph")
+        ),
+        a(href:="www.naver.com")(
+          p("NAVER")
+        ),
+        p(hidden)(
+          "I'm hidden"
         )
       )
     )
