d06d296d2a3745b5de025e67d8b70b1bded3113f
sangwoo <tkddn3367@gmail.com>
2023-07-02 10:01:32 +0900

    delete person

--- src/main/scala/DataService.scala
+++ src/main/scala/DataService.scala
@@ -9,6 +9,7 @@ class DataService(quill: Quill.Postgres[SnakeCase]) {
   def getPeople = run(query[Person])
   def add       = run(query[Person].insert(_.name -> lift("json"), _.age -> lift(30)))
   def update    = run(query[Person].filter(p => p.id == lift(3)).update(_.age -> lift(35)))
+  def delete    = run(query[Person].filter(p => p.name == lift("aaa")).delete)
 }
 
 object DataService {
@@ -21,13 +22,16 @@ object DataService {
   def update =
     ZIO.serviceWithZIO[DataService](_.update)
 
+  def delete =
+    ZIO.serviceWithZIO[DataService](_.delete)
+
   val live = ZLayer.fromFunction(new DataService(_))
 }
 
 object DataServiceMain extends ZIOAppDefault {
 
   override def run =
-    DataService.update
+    DataService.delete
       .provide(
         DataService.live,
         Quill.Postgres.fromNamingStrategy(SnakeCase),
