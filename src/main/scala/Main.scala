b52f4da2d4adb115a397aa85261d23a81a41833e
sangwoo <tkddn3367@gmail.com>
2023-07-02 22:51:05 +0900

    set rest api initial format

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,10 +1,25 @@
+import zhttp.http._
 import zhttp.service._
 import zio._
 
 object MainApp extends ZIOAppDefault {
-  def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] =
-    Server.start(
-      port = 8080,
-      http = GreetingApp() ++ RequestApp() ++ StatusApp()
-    )
+
+  val prog = for {
+    server <- ZIO.service[MyServer]
+    _ <- Server.start(8080, server.myApp)
+  } yield ()
+
+  override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] =
+    prog.provide(MyServer.layer)
+}
+
+class MyServer() {
+  val myApp = Http.collect[Request] {
+    case Method.GET -> _ / "api" / "people" =>
+      Response.text("people json")
+  }
+}
+
+object MyServer {
+  val layer = ZLayer.succeed(new MyServer())
 }
