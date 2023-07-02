36e942765b7362d15601ae705f1fb0405737c5d3
sangwoo <tkddn3367@gmail.com>
2023-07-02 09:51:13 +0900

    quill insert query

--- src/main/scala/DataService.scala
+++ src/main/scala/DataService.scala
@@ -1,4 +1,4 @@
-import io.getquill.SnakeCase
+import io.getquill._
 import io.getquill.jdbczio.Quill
 import zio.{ZIO, ZIOAppDefault, ZLayer}
 
@@ -7,11 +7,15 @@ case class Person(name: String, age: Int)
 class DataService(quill: Quill.Postgres[SnakeCase]) {
   import quill._
   def getPeople = run(query[Person])
+  def add = run(query[Person].insertValue(Person("soonjung", 40)))
 }
 
 object DataService {
   def getPeople =
     ZIO.serviceWithZIO[DataService](_.getPeople)
+
+  def add =
+    ZIO.serviceWithZIO[DataService](_.add)
 
   val live = ZLayer.fromFunction(new DataService(_))
 }
@@ -19,7 +23,7 @@ object DataService {
 object DataServiceMain extends ZIOAppDefault {
 
   override def run =
-    DataService.getPeople
+    DataService.add
       .provide(
         DataService.live,
         Quill.Postgres.fromNamingStrategy(SnakeCase),
