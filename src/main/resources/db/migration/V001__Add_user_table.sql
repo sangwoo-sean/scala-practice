e294565935bd66847dbfdb307bc49b946ebda938
sangwoo <tkddn3367@gmail.com>
2023-07-09 23:24:08 +0900

    flyway first db setting sql

--- src/main/resources/db/migration/V001__Add_user_table.sql
+++ src/main/resources/db/migration/V001__Add_user_table.sql
@@ -1,9 +1,9 @@
-create table user
+create table if not exists public.user
 (
     id   bigserial PRIMARY KEY,
     name varchar not null,
     age  integer
 );
 
-insert into user (id, name, age)
-values 1, 'admin', 0;
+insert into public.user (id, name, age)
+values (1, 'admin', 0);
