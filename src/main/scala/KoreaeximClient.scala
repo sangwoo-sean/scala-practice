926de04a80a5a57452e622dc9f68bc95d5f70726
sangwoo <tkddn3367@gmail.com>
2023-07-13 23:19:50 +0900

    get koreaexim 환율 with zio http client

---
+++ src/main/scala/KoreaeximClient.scala
@@ -0,0 +1,25 @@
+import zio._
+import zio.http._
+import zio.http.netty.NettyConfig
+import zio.http.netty.client.NettyClientDriver
+
+object KoreaeximClient extends ZIOAppDefault {
+  val url = "https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=3ltm76g7C0kGvXTEISglXjnFA7mEh6kC&searchdate=20230711&data=AP01"
+  val prog = for {
+    res <- Client.request(
+      url = url,
+      method = Method.GET
+    )
+    data <- res.body.asString
+    _ <- Console.printLine(data)
+  } yield()
+
+  val run: ZIO[Any, Throwable, Unit] =
+    prog.provide(
+      ZLayer.succeed(ZClient.Config.default),
+      Client.customized,
+      NettyClientDriver.live,
+      DnsResolver.default,
+      ZLayer.succeed(NettyConfig.default)
+    )
+}
