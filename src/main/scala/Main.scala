22cab49bdfa22a4daed36fddacd2353a5f88cbb7
sangwoo <tkddn3367@gmail.com>
2023-07-15 15:27:01 +0900

    read config

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,22 +2,38 @@ import io.getquill.SnakeCase
 import io.getquill.jdbczio.Quill
 import zhttp.service._
 import zio._
+import zio.config.magnolia.deriveConfig
+import zio.config.typesafe.TypesafeConfigProvider
 import zio.logging.backend.SLF4J
+
+case class HttpServerConfig(port: String, host: String, nThreads: Int)
+
+object HttpServerConfig {
+  val config = deriveConfig[HttpServerConfig].nested("HttpServerConfig")
+}
 
 object Main extends ZIOAppDefault {
 
-  override val bootstrap: ZLayer[Any, Any, Unit] = Runtime.removeDefaultLoggers >>> SLF4J.slf4j
+  override val bootstrap: ZLayer[Any, Any, Unit] =
+    Runtime.removeDefaultLoggers >>>
+      SLF4J.slf4j >>>
+      Runtime.setConfigProvider(TypesafeConfigProvider.fromResourcePath())
 
   val port = 8080
 
   val prog: ZIO[PersonEndPoint with DbMigrator, Throwable, Unit] = for {
     _ <- ZIO.logInfo("Start")
-    _ <- ZIO.logInfo("It's Scheduling!").repeat(Schedule.fixed(Duration.fromSeconds(60)).forever).fork
-    migrator <- ZIO.service[DbMigrator]
-    _ <- migrator.migrate
+
+    _ <- ZIO
+      .config[HttpServerConfig](HttpServerConfig.config)
+      .map(c => println(s"host: ${c.host}, port: ${c.port}, nThreds: ${c.nThreads}"))
+
+    _              <- ZIO.logInfo("It's Scheduling!").repeat(Schedule.fixed(Duration.fromSeconds(60)).forever).fork
+    migrator       <- ZIO.service[DbMigrator]
+    _              <- migrator.migrate
     personEndPoint <- ZIO.service[PersonEndPoint]
     _              <- Server.start(port, personEndPoint.endpoint)
-    _ <- ZIO.logInfo("Done")
+    _              <- ZIO.logInfo("Done")
   } yield ()
 
   override def run: ZIO[Any, Throwable, Unit] =
