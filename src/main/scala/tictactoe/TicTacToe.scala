274c3a7404edaddb5e43f9391527b5b979e57ed2
sangwoo <tkddn3367@gmail.com>
2023-07-25 23:03:15 +0900

    Revert "tictactoe environment vertical composition & horizontal composition"
    This reverts commit a2e417e6fbbde8b09ba4ba58cce4c510012fca55.

--- src/main/scala/tictactoe/TicTacToe.scala
+++ src/main/scala/tictactoe/TicTacToe.scala
@@ -1,20 +1,20 @@
 package tictactoe
 
-import tictactoe.controller.{Controller, ControllerLive}
+import tictactoe.controller.ControllerLive
 import tictactoe.domain.State
-import tictactoe.domain.gameLogic.{GameLogic, GameLogicLive}
-import tictactoe.mode.confirm.{ConfirmMode, ConfirmModeLive}
-import tictactoe.mode.game.{GameMode, GameModeLive}
-import tictactoe.mode.menu.{MenuMode, MenuModeLive}
-import tictactoe.opponentAi.{OpponentAi, OpponentAiLive}
-import tictactoe.parser.confirm.game.{ConfirmCommandParser, ConfirmCommandParserLive}
-import tictactoe.parser.game.{GameCommandParser, GameCommandParserLive}
-import tictactoe.parser.menu.{MenuCommandParser, MenuCommandParserLive}
+import tictactoe.domain.gameLogic.GameLogicLive
+import tictactoe.mode.confirm.ConfirmModeLive
+import tictactoe.mode.game.GameModeLive
+import tictactoe.mode.menu.MenuModeLive
+import tictactoe.opponentAi.OpponentAiLive
+import tictactoe.parser.confirm.game.ConfirmCommandParserLive
+import tictactoe.parser.game.GameCommandParserLive
+import tictactoe.parser.menu.MenuCommandParserLive
 import tictactoe.runLoop.{RunLoop, RunLoopLive}
 import tictactoe.terminal.TerminalLive
-import tictactoe.view.confirm.{ConfirmView, ConfirmViewLive}
-import tictactoe.view.game.{GameView, GameViewLive}
-import tictactoe.view.menu.{MenuView, MenuViewLive}
+import tictactoe.view.confirm.ConfirmViewLive
+import tictactoe.view.game.GameViewLive
+import tictactoe.view.menu.MenuViewLive
 import zio._
 
 object TicTacToe extends ZIOAppDefault {
@@ -30,28 +30,22 @@ object TicTacToe extends ZIOAppDefault {
     loop(State.initial)
   }
 
-  val run = program.provide(environmentLayer)
-
-  private lazy val environmentLayer: ULayer[RunLoop] = {
-
-    val confirmModeDeps: ULayer[ConfirmCommandParser with ConfirmView] =
-      ConfirmCommandParserLive.layer ++ ConfirmViewLive.layer
-    val menuModeDeps: ULayer[MenuCommandParser with MenuView] =
-      MenuCommandParserLive.layer ++ MenuViewLive.layer
-    val gameModeDeps: ULayer[GameCommandParser with GameView with GameLogic with OpponentAi] =
-      GameCommandParserLive.layer ++ GameViewLive.layer ++ GameLogicLive.layer ++ OpponentAiLive.layer
-
-    val confirmModeNoDeps: ULayer[ConfirmMode] = confirmModeDeps >>> ConfirmModeLive.layer
-    val menuModeNoDeps: ULayer[MenuMode] = menuModeDeps >>> MenuModeLive.layer
-    val gameModeNoDeps: ULayer[GameMode] = gameModeDeps >>> GameModeLive.layer
-
-    val controllerDeps: ULayer[ConfirmMode with GameMode with MenuMode] =
-      confirmModeNoDeps ++ menuModeNoDeps ++ gameModeNoDeps
-
-    val controllerNoDeps: ULayer[Controller] = controllerDeps >>> ControllerLive.layer
-
-    val runLoopNoDeps = (controllerNoDeps ++ TerminalLive.layer) >>> RunLoopLive.layer
-
-    runLoopNoDeps
-  }
+  val run =
+    program
+      .provide(
+        ControllerLive.layer,
+        GameLogicLive.layer,
+        ConfirmModeLive.layer,
+        GameModeLive.layer,
+        MenuModeLive.layer,
+        OpponentAiLive.layer,
+        ConfirmCommandParserLive.layer,
+        GameCommandParserLive.layer,
+        MenuCommandParserLive.layer,
+        RunLoopLive.layer,
+        TerminalLive.layer,
+        ConfirmViewLive.layer,
+        GameViewLive.layer,
+        MenuViewLive.layer
+      )
 }
