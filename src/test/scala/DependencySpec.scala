c243641aca490500a698f8fb60884a3c2f235af6
sangwoo <tkddn3367@gmail.com>
2023-09-11 23:38:03 +0900

    ZLayer dependency Spec: vertical composition

--- src/test/scala/DependencySpec.scala
+++ src/test/scala/DependencySpec.scala
@@ -1,12 +1,13 @@
 import zio._
-import zio.test.{Spec, TestEnvironment, ZIOSpecDefault, assertTrue}
+import zio.test.{assertTrue, Spec, TestEnvironment, ZIOSpecDefault}
 
 object DependencySpec extends ZIOSpecDefault {
+
   override def spec: Spec[TestEnvironment with Scope, Any] =
-    suite("dependency") (
+    suite("dependency")(
       test("service") {
         for {
-          a <- ZIO.service[A]
+          a   <- ZIO.service[A]
           res <- a.foo
         } yield assertTrue(res == "Hello!")
       },
@@ -22,12 +23,25 @@ object DependencySpec extends ZIOSpecDefault {
           bar <- ZIO.serviceWithZIO[B](_.bar)
         } yield assertTrue(foo == "Hello!" && bar == 42)
       },
-    ).provide(A.layer ++ B.layer)
+      test("Vertical Composition") {
+        for {
+          res <- ZIO.serviceWithZIO[C](_.baz)
+        } yield assertTrue(res == "Hello!42")
+      }
+    ).provide(
+      A.layer,
+      B.layer,
+      (A.layer ++ B.layer) >>>
+        C.layer,
+//      C.layer2
+    )
+
 }
 
 final class A {
   def foo: UIO[String] = ZIO.succeed("Hello!")
 }
+
 object A {
   val layer: ULayer[A] = ZLayer.succeed(new A)
 }
@@ -35,6 +49,30 @@ object A {
 final class B {
   def bar: UIO[Int] = ZIO.succeed(42)
 }
+
 object B {
   val layer: ULayer[B] = ZLayer.succeed(new B)
+}
+
+final case class C(a: A, b: B) {
+
+  def baz: ZIO[Any, Nothing, String] =
+    for {
+      f <- a.foo
+      b <- b.bar
+    } yield f + b.toString
+
+}
+
+object C {
+  def baz = ZIO.serviceWithZIO[C](_.baz)
+
+  val layer: ZLayer[B with A, Nothing, C] = ZLayer {
+    for {
+      a <- ZIO.service[A]
+      b <- ZIO.service[B]
+    } yield C(a, b)
+  }
+
+  val layer2: ULayer[C] = ZLayer.succeed(C(new A, new B))
 }
