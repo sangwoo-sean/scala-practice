b03babc10e31d478a83f74f471f75ea0364cc950
Sean <sangwoo.park@doomoolmori.com>
2023-09-28 19:31:22 +0900

    ZIO Prelude: NewType

--- src/test/scala/ZIOPreludeSpec.scala
+++ src/test/scala/ZIOPreludeSpec.scala
@@ -3,6 +3,8 @@ import zio.test.{Spec, TestEnvironment, ZIOSpecDefault, assertTrue}
 import zio.prelude._
 import zio.prelude.newtypes._
 
+import scala.util.Try
+
 case class Topic(value: String)
 case class Votes(value: Int)
 
@@ -11,6 +13,7 @@ object Votes {
   implicit val VotesAssociative: Associative[Votes] = new Associative[Votes] {
     override def combine(l: => Votes, r: => Votes): Votes = Votes(l.value + r.value)
   }
+
   implicit val VotesCommutative: Commutative[Votes] = new Commutative[Votes] {
     override def combine(l: => Votes, r: => Votes): Votes = Votes(l.value + r.value)
   }
@@ -22,6 +25,10 @@ case class VoteMap(map: Map[Topic, Votes])
 object VoteMap {
   def combine(l: VoteMap, r: VoteMap): VoteMap = VoteMap(l.map <> r.map)
 }
+
+/** A new type in ZIO Prelude is "a type that has the same underlying representation as another type at RUNTIME" but is
+  * "a separate type at COMPILE TIME"
+  */
 
 object ZIOPreludeSpec extends ZIOSpecDefault {
 
@@ -44,7 +51,7 @@ object ZIOPreludeSpec extends ZIOSpecDefault {
           val result = Votes(3) <> Votes(2)
           assertTrue(result == Votes(5))
         },
-        suite("newtype") (
+        suite("newtype")(
           test("sum") {
             val result = Sum(1) <> Sum(2)
             assertTrue(result == Sum(3))
@@ -72,7 +79,34 @@ object ZIOPreludeSpec extends ZIOSpecDefault {
             assertTrue(result == Max(2))
           },
         )
-      )
+      ) +
+        suite("New Types")(
+          test("normal types can be interchangeable") {
+            type ANumber = Int
+            type SNumber = Int
+            def takeAnum(num: ANumber) = num
+
+            val anum: ANumber = 1
+            val snum: SNumber = 1
+
+            assertTrue(anum == snum && takeAnum(anum) == anum && takeAnum(snum) == snum)
+          },
+          test("NewType makes same type different") {
+            object AccountNumber extends Newtype[Int]
+            type AccountNumber = AccountNumber.Type
+            object SequenceNumber extends Newtype[Int]
+            type SequenceNumber = SequenceNumber.Type
+            def takeAnumNewType(num: AccountNumber) = num
+
+            val anumNewType = AccountNumber(1)
+            val snumNewType = SequenceNumber(1)
+
+            val isSame = anumNewType == snumNewType // this will be evaluated in runtime. so it works.
+            //takeAnumNewType(snumNewType) //type mismatch
+//            assertTrue(anumNewType == snumNewType)// this will be evaluated in compile time. so type mismatch
+            assertTrue(takeAnumNewType(anumNewType) == anumNewType && isSame)
+          }
+        )
     )
 
 }
