39059d3f217b91383bfb5388272ee38ed7293e87
Sean <sangwoo.park@doomoolmori.com>
2023-10-07 21:08:44 +0900

    currying

--- src/test/scala/BasicOperationSpec.scala
+++ src/test/scala/BasicOperationSpec.scala
@@ -53,7 +53,7 @@ object BasicOperationSpec extends ZIOSpecDefault {
         assertTrue(true)
       },
       test("Map withDefaultValue") {
-        val prevMap              = Map(1 -> "a", 2 -> "b", 3 -> "c")
+        val prevMap = Map(1 -> "a", 2 -> "b", 3 -> "c")
 
         val applyContainingValue = prevMap(3)
         val getContainingValue   = prevMap.get(3)
@@ -73,6 +73,23 @@ object BasicOperationSpec extends ZIOSpecDefault {
             withDefaultValueMap.size == 3 &&
             applyNoMappingValue == "empty" &&
             getNoMappingValue2.isEmpty
+        )
+      },
+      test("currying") {
+        def funcA(a: Int, b: Int) = a + b
+        def funcB(a: Int)(b: Int) = a + b
+
+        // 같은 인자를 주고, 내부적으로도 같은 함수(funcD) 를 사용하지만 결과를 다르게 할 수 있다.
+        // 내부에서 비슷한 로직을 사용하고 인자도 같지만, 일부분 다른 로직을 사용할 때 고려해볼 수 있다.
+        def funcC1(a: Int)        = funcD(a, 1)
+        def funcC2(a: Int)        = funcD(a, 2)
+        def funcD(a: Int, b: Int) = a + b
+
+        assertTrue(
+          funcA(1, 2) == 3 &&
+            funcB(1)(2) == 3 &&
+            funcC1(1) == 2 &&
+            funcC2(1) == 3
         )
       }
     )
