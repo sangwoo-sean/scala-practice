c60346c8af063f9e10c4159e45c2c7508fcf30a6
sangwoo <tkddn3367@gmail.com>
2024-02-28 22:27:55 +0900

    add Doc to query & path parameter

--- src/main/scala/openapi/ZioHttpApp.scala
+++ src/main/scala/openapi/ZioHttpApp.scala
@@ -3,15 +3,15 @@ package openapi
 import zio._
 import zio.http._
 import zio.http.codec.HttpCodec.query
-import zio.http.codec.PathCodec
+import zio.http.codec.{Doc, PathCodec}
 import zio.http.endpoint._
 import zio.http.endpoint.openapi.OpenAPIGen
 
 object ZioHttpApp extends ZIOAppDefault {
 
   val getEndpoint =
-    Endpoint(Method.GET / "users" / int("userId") / "posts" / int("postId"))
-      .query(query("name"))
+    Endpoint(Method.GET / "users" / int("userId") ?? Doc.p("""this is "userId" path parameter  """))
+      .query(query("name").optional ?? Doc.p("""this is "name" query parameter """))
       .out[List[String]]
 
   val openAPI = OpenAPIGen.fromEndpoints(title = "Endpoint Example", version = "1.0", getEndpoint)
@@ -22,14 +22,19 @@ object ZioHttpApp extends ZIOAppDefault {
 
   val routes = Routes(
     getEndpoint.implement {
-      Handler.fromFunctionZIO[(Int, Int, String)] {
-        case (userId, postId, name) =>
-          ZIO.succeed(List(s"$userId", s"$postId", name))
+      Handler.fromFunctionZIO[(Int, Option[String])] {
+        case (userId, Some(name)) =>
+          ZIO.succeed(List(s"$userId", name))
       }
     }
   ) ++ docRoute
 
   val app = routes.toHttpApp
+
   override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] =
-    Server.serve(app).provide(Server.defaultWithPort(8082))
+    Server
+      .install(app)
+      .flatMap(port => ZIO.logInfo(s"Listening on port $port") *> ZIO.never)
+      .provide(Server.defaultWithPort(8082))
+
 }
